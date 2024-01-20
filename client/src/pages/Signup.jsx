ce43f33237ebfb0700a82da4a7dcb63174aea0ef
DimuthuShasantha <mdimuthusw@gmail.com>
2024-01-20 23:08:33 +0530

    add functionality to the signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,30 +1,64 @@
-import { Link } from "react-router-dom";
-import { Button, Label, TextInput } from "flowbite-react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button, Label, TextInput, Alert, Spinner } from "flowbite-react";
+import { HiInformationCircle } from "react-icons/hi";
+import { useState } from "react";
 
 export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.password || !formData.email)
+      return setErrorMessage("Please fill out all feilds!");
+    try {
+      setErrorMessage(null);
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (res.ok) {
+        navigate("/signin");
+      }
+      if (data.success === false) return setErrorMessage(data.message);
+    } catch (error) {
+      setLoading(false);
+      setErrorMessage(error.message);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20 ">
       <div className="flex flex-col max-w-3xl gap-5 mx-auto md:flex-row md:items-center">
         <div className="flex-1">
-          <Link
-            to="/"
-            className="text-4xl font-bold dark:text-white"
-          >
+          <Link to="/" className="text-4xl font-bold dark:text-white">
             <span className="px-2 py-1 text-white rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
               Dimu's
             </span>
             Blog
           </Link>
-          <p className="mt-5 text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet quidem ratione optio !</p>
+          <p className="mt-5 text-sm">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+            quidem ratione optio !
+          </p>
         </div>
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
             <div>
               <Label value="Your Username" />
               <TextInput
                 type="text"
                 id="username"
                 placeholder="Username"
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -33,6 +67,7 @@ export default function Signup() {
                 type="email"
                 id="email"
                 placeholder="Email Address"
+                onChange={handleChange}
               />
             </div>
             <div>
@@ -41,10 +76,22 @@ export default function Signup() {
                 type="password"
                 id="password"
                 placeholder="password"
+                onChange={handleChange}
               />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign Up
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign up"
+              )}
             </Button>
           </form>
           <div className="flex justify-center gap-2 mt-3 text-sm">
@@ -53,6 +100,15 @@ export default function Signup() {
               Sign in
             </Link>
           </div>
+          {errorMessage && (
+            <Alert
+              className="py-3 mt-5"
+              color="failure"
+              icon={HiInformationCircle}
+            >
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
