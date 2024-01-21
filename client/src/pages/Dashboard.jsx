d2a6b50c1565d941f1c4ca2f47d4da16b6847e55
DimuthuShasantha <mdimuthusw@gmail.com>
2024-01-21 19:21:56 +0530

    complete profile page ui

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -13,8 +13,10 @@ export default function Dashboard() {
   }, [location.search]);
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
-      <div className="md:w-56"><DashSidebar /></div>
-      {tab === "profile" && <div><DashProfile /></div>}
+      <div className="md:w-56">
+        <DashSidebar />
+      </div>
+      {tab === "profile" && <DashProfile />}
     </div>
   );
 }
