249bbff2dce595bbed2e5dc61caef85e397e9420
DimuthuShasantha <mdimuthusw@gmail.com>
2024-01-21 10:49:34 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -23,6 +24,26 @@ export const signup = async (req, res, next) => {
     });
     await newUser.save();
     res.status(201).json({ message: "User saved successfully!" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password || username === "" || password === "")
+      return next(errorHandler(400, "All feilds are required!"));
+    const validUser = await User.findOne({ username });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(400, "Invalid credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc
+    res
+      .status(200)
+      .cookie("ACCESS_TOKEN", token, { httpOnly: true })
+      .json(rest);
   } catch (error) {
     next(error);
   }
