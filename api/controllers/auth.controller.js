1f2d52c0bbf0958f85178acab5e47e39dac502d1
DimuthuShasantha <mdimuthusw@gmail.com>
2024-01-22 21:07:35 +0530

    add admin functionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -38,7 +38,7 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, "User not found!"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(400, "Invalid credentials!"));
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc
     res
       .status(200)
@@ -54,7 +54,7 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+      const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET)
       const { password, ...rest } = user._doc;
       res
       .status(200)
@@ -70,7 +70,7 @@ export const google = async (req, res, next) => {
         avatar: googlePhotoURL
       })
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET)
+      const token = jwt.sign({ id: newUser._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET)
       const { password, ...rest } = newUser._doc;
       res
       .status(200)
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema(
     avatar: {
       type: String,
       default: "https://img.freepik.com/free-photo/portrait-smiling-blonde-woman_23-2148316635.jpg?w=740&t=st=1705830725~exp=1705831325~hmac=818925358a1f4c4098339bb59e4428ec47fd33441948cb0ee24e7c4254b8bdba"
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false
     }
   },
   { timestamps: true }
