a5c580bbda94184c62b71c2055433a8cb02ad9b1
DimuthuShasantha <mdimuthusw@gmail.com>
2024-03-20 21:27:08 +0530

    complete project and about pages

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,11 @@
+import CallToAction from '../components/CallToAction';
+
 export default function Projects() {
   return (
-    <div>
-      Projects
+    <div className='flex flex-col items-center justify-center max-w-2xl min-h-screen gap-6 p-3 mx-auto'>
+      <h1 className='text-3xl font-semibold'>Pojects</h1>
+      <p className='text-gray-500 text-md'>Build fun and engaging projects while learning HTML, CSS, and JavaScript!</p>
+      <CallToAction />
     </div>
   )
 }
